bf093cf08a3ce9f08e958633f7d2d541c9ae0e34
Jesse C <straynamic@gmail.com>
2019-08-19 21:59:55 -0700

    test123

--- PizzaSite/PizzaSite/_javascript/HideElement.js
+++
@@ -1,8 +0,0 @@
-﻿function HE_ToggleHide() {
-  var x = document.getElementById("HideableElement");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
